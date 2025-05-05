7e2d943c42ca4e0202fa36876126b8258cca693c
danielemurgioni <dashomew@gmail.com>
2025-05-05 18:58:16 +0200

    [UPD] create main structure

---
+++ src/components/Main.jsx
@@ -0,0 +1,20 @@
+const Main = () => {
+    return (
+        <main>
+            <div className="content">
+                <h3>Content goes here</h3>
+            </div>
+            <div className="shop">
+                <ul>
+                    <li><img src="../src/assets/img/buy-comics-digital-comics.png" /><a href="#">digital comics</a></li>
+                    <li><img src="../src/assets/img/buy-comics-merchandise.png" /><a href="#">dc merchandise</a></li>
+                    <li><img src="../src/assets/img/buy-comics-subscriptions.png" /><a href="#">subscription</a></li>
+                    <li><img src="../src/assets/img/buy-comics-shop-locator.png" /><a href="#">comic shop locator</a></li>
+                    <li><img src="../src/assets/img/buy-dc-power-visa.svg" /><a href="#">dc power visa</a></li>
+                </ul>
+            </div>
+        </main>
+    );
+};
+
+export default Main;
