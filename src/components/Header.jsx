14368bbfc988b930eb4c9c121a2c34093669fe69
danielemurgioni <dashomew@gmail.com>
2025-05-05 18:42:10 +0200

    [UPD] create header structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+const Header = () => {
+    return (
+        <header className="d-flex">
+            <figure>
+                <img src="../src/assets/img/dc-logo.png" alt="Dc Comics" />
+            </figure>
+            <nav>
+                <ul>
+                    <li><a href="#">characters</a></li>
+                    <li><a href="#">comics</a></li>
+                    <li><a href="#">movies</a></li>
+                    <li><a href="#">tv</a></li>
+                    <li><a href="#">games</a></li>
+                    <li><a href="#">collectibles</a></li>
+                    <li><a href="#">videos</a></li>
+                    <li><a href="#">fans</a></li>
+                    <li><a href="#">news</a></li>
+                    <li><a href="#">shop</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
