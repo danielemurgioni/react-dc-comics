d1fe05786e9f84a8e22204641389880d1e00eb54
danielemurgioni <dashomew@gmail.com>
2025-05-06 17:12:06 +0200

    [UPD] changed footer structure

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,63 +2,74 @@ const Footer = () => {
     return (
         <footer>
             <div id="top-footer">
-                <div>
-                    <h2>dc comics</h2>
-                    <ul>
-                        <li><a href="#">Characters</a></li>
-                        <li><a href="#">Comics</a></li>
-                        <li><a href="#">Movies</a></li>
-                        <li><a href="#">TV</a></li>
-                        <li><a href="#">Games</a></li>
-                        <li><a href="#">Videos</a></li>
-                        <li><a href="#">News</a></li>
-                    </ul>
-                </div>
-                <div>
-                    <h2>dc</h2>
-                    <ul>
-                        <li><a href="#">Terms Of Use</a></li>
-                        <li><a href="#">Privacy Policy (New)</a></li>
-                        <li><a href="#">Ad Choices</a></li>
-                        <li><a href="#">Advertising</a></li>
-                        <li><a href="#">Jobs</a></li>
-                        <li><a href="#">Subscriptions</a></li>
-                        <li><a href="#">Talent Workshops</a></li>
-                        <li><a href="#">CPSC Certificates</a></li>
-                        <li><a href="#">Ratings</a></li>
-                        <li><a href="#">Shop Help</a></li>
-                        <li><a href="#">Contant Us</a></li>
-                    </ul>
-                </div>
-                <div>
-                    <h2>sites</h2>
-                    <ul>
-                        <li><a href="#">DC</a></li>
-                        <li><a href="#">MAD Magazine</a></li>
-                        <li><a href="#">DC Kids</a></li>
-                        <li><a href="#">DC Universe</a></li>
-                        <li><a href="#">DC Power Visa</a></li>
-                    </ul>
-                </div>
-                <div>
-                    <h2>shop</h2>
-                    <ul>
-                        <li><a href="#">Shop DC</a></li>
-                        <li><a href="#">Shop DC Collectibles</a></li>
-                    </ul>
-                </div>
-                <div className="bg-dc-logo">
-
+                <div className="container">
+                    <div className="d-flex">
+                        <div className="info-links">
+                            <h2>dc comics</h2>
+                            <ul>
+                                <li><a href="#">Characters</a></li>
+                                <li><a href="#">Comics</a></li>
+                                <li><a href="#">Movies</a></li>
+                                <li><a href="#">TV</a></li>
+                                <li><a href="#">Games</a></li>
+                                <li><a href="#">Videos</a></li>
+                                <li><a href="#">News</a></li>
+                            </ul>
+                        </div>
+                        <div className="info-links">
+                            <h2>dc</h2>
+                            <ul>
+                                <li><a href="#">Terms Of Use</a></li>
+                                <li><a href="#">Privacy Policy (New)</a></li>
+                                <li><a href="#">Ad Choices</a></li>
+                                <li><a href="#">Advertising</a></li>
+                                <li><a href="#">Jobs</a></li>
+                                <li><a href="#">Subscriptions</a></li>
+                                <li><a href="#">Talent Workshops</a></li>
+                                <li><a href="#">CPSC Certificates</a></li>
+                                <li><a href="#">Ratings</a></li>
+                                <li><a href="#">Shop Help</a></li>
+                                <li><a href="#">Contant Us</a></li>
+                            </ul>
+                        </div>
+                        <div className="info-links">
+                            <h2>sites</h2>
+                            <ul>
+                                <li><a href="#">DC</a></li>
+                                <li><a href="#">MAD Magazine</a></li>
+                                <li><a href="#">DC Kids</a></li>
+                                <li><a href="#">DC Universe</a></li>
+                                <li><a href="#">DC Power Visa</a></li>
+                            </ul>
+                        </div>
+                        <div className="info-links">
+                            <h2>shop</h2>
+                            <ul>
+                                <li><a href="#">Shop DC</a></li>
+                                <li><a href="#">Shop DC Collectibles</a></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div id="bottom-footer">
-                <button>sing-up now!</button>
-                <h2>follow us</h2>
-                <img src="../src/assets/img/footer-facebook.png" alt="facebook" />
-                <img src="../src/assets/img/footer-twitter.png" alt="twitter" />
-                <img src="../src/assets/img/footer-youtube.png" alt="youtube" />
-                <img src="../src/assets/img/footer-pinterest.png" alt="pinterest" />
-                <img src="../src/assets/img/footer-periscope.png" alt="periscope" />
+                <div className="container">
+                    <div className="d-flex">
+                        <div className="col-20">
+                            <button>sing-up now!</button>
+                        </div>
+                        <div className="col-80">
+                            <div className="d-flex justify-end">
+                                <h2>follow us</h2>
+                                <img src="/src/assets/img/footer-facebook.png" alt="facebook" />
+                                <img src="/src/assets/img/footer-twitter.png" alt="twitter" />
+                                <img src="/src/assets/img/footer-youtube.png" alt="youtube" />
+                                <img src="/src/assets/img/footer-pinterest.png" alt="pinterest" />
+                                <img src="/src/assets/img/footer-periscope.png" alt="periscope" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </footer>
     );
