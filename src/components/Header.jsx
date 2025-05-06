780c378be53bdda2ac9b9aaac9d85dc9b2747cb6
danielemurgioni <dashomew@gmail.com>
2025-05-06 10:19:48 +0200

    [UPD] changed header structure

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,31 @@
 const Header = () => {
     return (
-        <header className="d-flex">
-            <figure>
-                <img src="../src/assets/img/dc-logo.png" alt="Dc Comics" />
-            </figure>
-            <nav>
-                <ul>
-                    <li><a href="#">characters</a></li>
-                    <li><a href="#">comics</a></li>
-                    <li><a href="#">movies</a></li>
-                    <li><a href="#">tv</a></li>
-                    <li><a href="#">games</a></li>
-                    <li><a href="#">collectibles</a></li>
-                    <li><a href="#">videos</a></li>
-                    <li><a href="#">fans</a></li>
-                    <li><a href="#">news</a></li>
-                    <li><a href="#">shop</a></li>
-                </ul>
-            </nav>
+        <header>
+            <div className="container p-20">
+                <div className="d-flex align-center">
+                    <div className="col-20">
+                        <figure>
+                            <img src="../src/assets/img/dc-logo.png" alt="Dc Comics" />
+                        </figure>
+                    </div>
+                    <div className="col-80">
+                        <nav>
+                            <ul className="d-flex justify-end">
+                                <li><a href="#">characters</a></li>
+                                <li><a href="#">comics</a></li>
+                                <li><a href="#">movies</a></li>
+                                <li><a href="#">tv</a></li>
+                                <li><a href="#">games</a></li>
+                                <li><a href="#">collectibles</a></li>
+                                <li><a href="#">videos</a></li>
+                                <li><a href="#">fans</a></li>
+                                <li><a href="#">news</a></li>
+                                <li><a href="#">shop</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
         </header>
     );
 };
